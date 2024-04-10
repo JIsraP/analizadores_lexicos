4a7a6ada57a298f661fef9c4441443f4bb87d18b
JIsraP <a01423294@tec.mx>
2024-04-09 18:00:02 -0600

    Conditional lexer ex for string test cases

--- clite.py
+++ clite.py
@@ -1,6 +1,11 @@
 import ply.lex as lex
 
-tokens = [ 'INT', 'FLOAT', 'STR' ]
+# Declare the states
+states = (
+    ('string', 'exclusive'),
+)
+
+tokens = [ 'INT', 'FLOAT', 'STR', 'STRCONTENT', 'QUOTATION' ]
 
 t_ignore  = ' \t'
 
@@ -8,10 +13,29 @@ def getLexer():
     lexer = lex.lex()
     return lexer
 
-def t_STR(t):
-    r'.\"*[aA-zZ]+(\s.+)*.\"*'
-    t.value = str(t.value)
+# def t_STR(t):
+#     r'.\"*[aA-zZ]+(\s.+)*.\"*'
+#     t.value = str(t.value)
+#     return t
+
+# Regular expression rules for the initial state
+def t_string(t):
+    r'\"'
+    t.lexer.str_start = t.lexer.lexpos
+    t.lexer.begin('string')
+
+def t_string_STR(t):
+    r'\"'
+    t.value = t.lexer.lexdata[t.lexer.str_start-1:t.lexer.lexpos]
+    t.type = "STR"
+    t.lexer.begin('INITIAL')
     return t
+
+def t_string_QUOTATION(t):
+    r'\\"'
+
+def t_string_STRCONTENT(t):
+    r'[^"]'
 
 def t_FLOAT(t):
     r'([0-9]_?[0-9]+|[0-9]*)\.[0-9]*([eE]?[\+-]?([0-9]_?[0-9]*)+)?|[0-9]+[eE][\+-]?([0-9]_?[0-9]*)+'
